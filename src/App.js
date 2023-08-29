9113e40084941ede5931001c8084b6236f82778f
Rohi29 <rohiniphatangaren5@gmail.com>
2023-08-29 17:02:06 +0530

    added logic of code

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ import './App.css';
 
 const App = () => {
 
-  const board = ["X","X","X","X","X","X","X","X","X"]
+  const [xPlaying, setXPlaying] = useState(true);
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const handleBoxClick = (boxIdx) => {
+    // Step 1: Update the board
+    const updatedBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
+        return xPlaying ? "X" : "O";
+      } else {
+        return value;
+      }
+    })
+
+    setBoard(updatedBoard);
+
+  
+
+    
+    setXPlaying(!xPlaying);
+  }
 
   return (
     <div className="App">
-      <Board board={board} onClick={null} />
+      <Board board={board} onClick={handleBoxClick} />
       
     </div>
   );
